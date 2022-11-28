6047e607d449af3fe3d754c65861ab6ee2c42599
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-11-28 22:28:17 +0300

    day00: ex05

---
+++ day00/src/ex05/day00_ex05.sql
@@ -0,0 +1,15 @@
+SELECT
+    (
+        SELECT name
+        FROM person
+        WHERE id = person_order.person_id
+    )
+FROM person_order
+WHERE (menu_id = 13
+    OR menu_id = 14
+    OR menu_id = 18)
+    AND order_date = '2022-01-07';
+
+
+-- https://www.ibm.com/docs/en/informix-servers/14.10?topic=statements-subqueries-in-projection-clause
+-- https://www.sqltutorial.org/sql-correlated-subquery/
