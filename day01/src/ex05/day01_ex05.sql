e12d0e30c5ae95b871808a3391f70f4c07249e79
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-11-29 22:45:42 +0300

    day01: ex05

---
+++ day01/src/ex05/day01_ex05.sql
@@ -0,0 +1,12 @@
+-- certainly `SELECT *` will do, but for the clarity sake...
+SELECT
+    person.id AS "person.id",
+    person.name AS "person.name",
+    person.age AS "person.age",
+    person.gender AS "person.gender",
+    person.address AS "person.address",
+    pizzeria.id AS "pizzeria.id",
+    pizzeria.name AS "pizzeria.name",
+    pizzeria.rating AS "pizzeria.rating"
+FROM person, pizzeria;
+ORDER BY person.id, pizzeria.id;
