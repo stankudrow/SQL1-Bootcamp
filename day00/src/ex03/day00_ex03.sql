04c23f8a76fae100e70429d8245315a2e6cd87e6
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-11-30 20:16:07 +0300

    day00: ex03 - revisited

--- day00/src/ex03/day00_ex03.sql
+++ day00/src/ex03/day00_ex03.sql
@@ -1,4 +1,7 @@
 SELECT DISTINCT person_id
 FROM person_visits
-WHERE (visit_date = '2022-01-06' AND visit_date = '2022-01-09') OR pizzeria_id = 2
+WHERE (
+    visit_date BETWEEN '2022-01-06' AND '2022-01-09'
+    )
+    OR pizzeria_id = 2
 ORDER BY person_id DESC;
