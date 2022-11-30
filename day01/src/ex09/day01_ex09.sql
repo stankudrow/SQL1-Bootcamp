62b1c9bc2c1398ca4e9dee35648cd78654b6df8c
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-11-30 20:54:44 +0300

    day01: ex09

---
+++ day01/src/ex09/day01_ex09.sql
@@ -0,0 +1,14 @@
+SELECT name
+FROM pizzeria
+WHERE id NOT IN (
+    SELECT DISTINCT pizzeria_id
+    FROM person_visits
+);
+
+SELECT name
+FROM pizzeria AS p
+WHERE NOT EXISTS (
+    SELECT *
+    FROM person_visits AS pv
+    WHERE pv.pizzeria_id = p.id
+);
