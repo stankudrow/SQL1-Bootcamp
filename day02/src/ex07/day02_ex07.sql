68de1a7122427566a6e31299dfb4c3b338b91bd2
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-12-01 18:51:27 +0300

    day02: ex07

---
+++ day02/src/ex07/day02_ex07.sql
@@ -0,0 +1,11 @@
+SELECT pz.name AS pizzeria_name
+FROM pizzeria AS pz
+JOIN person_visits AS pv
+ON pz.id = pv.pizzeria_id
+JOIN person AS p
+ON pv.person_id = p.id
+JOIN menu AS m
+ON pz.id = m.pizzeria_id
+WHERE pv.visit_date = '2022-01-08'
+    AND p.name = 'Dmitriy'
+    AND m.price < 800;
