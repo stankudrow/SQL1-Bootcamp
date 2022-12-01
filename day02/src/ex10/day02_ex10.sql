f570a4860895e6e4ebbb3c905646563f25318fe0
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-12-01 23:02:22 +0300

    day02: ex10

---
+++ day02/src/ex10/day02_ex10.sql
@@ -0,0 +1,9 @@
+SELECT
+    p1.name AS person_name1,
+    p2.name AS person_name2,
+    p2.address AS common_address
+FROM person AS p1, person AS p2
+WHERE p1.name != p2.name
+    AND p1.address = p2.address
+    AND p1.id > p2.id
+ORDER BY 1, 2, 3;
