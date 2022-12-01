93c68071b00711b439f6b265c5731fd9e3f4c4f1
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-12-01 21:43:56 +0300

    day01: ex01 - fixed

--- day01/src/ex01/day01_ex01.sql
+++ day01/src/ex01/day01_ex01.sql
@@ -1,6 +1,9 @@
-SELECT pizza_name AS object_name
-FROM menu
-UNION ALL
-SELECT name AS object_name
-FROM person
-ORDER BY object_name;
+SELECT uni.object_name
+FROM (
+    SELECT pizza_name AS object_name, 'menu' AS label
+    FROM menu
+    UNION ALL
+    SELECT name AS object_name, 'person' AS label
+    FROM person
+    ORDER BY label DESC
+) AS uni;
