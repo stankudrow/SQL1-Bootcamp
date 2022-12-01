4134cce5f07d9a3b7b1fb40c1c563aadc2959222
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-12-01 18:38:40 +0300

    day02: ex06 - explicity added

--- day02/src/ex06/day02_ex06.sql
+++ day02/src/ex06/day02_ex06.sql
@@ -3,7 +3,9 @@ FROM person_order AS po
 JOIN menu AS m
 ON m.id = po.menu_id
 JOIN pizzeria AS pz
-ON m.pizzeria_id = pz.id 
-WHERE po.person_id = 1 OR po.person_id = 4
+ON m.pizzeria_id = pz.id
+JOIN person AS p
+ON p.id = po.person_id
+WHERE p.name = 'Anna' OR p.name = 'Denis'
 ORDER BY 1, 2;
 
